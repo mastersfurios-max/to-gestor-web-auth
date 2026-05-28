dc46f0528716e3db97321b023c3d03fdf0081d11
mastersfurios-max <mastersfurios@gmail.com>
2026-05-28 17:47:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/to-gestor-web-auth/',
+  plugins: [tailwindcss(), react()],
 })
